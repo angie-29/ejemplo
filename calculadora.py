8a76ea3493aae71e265afb347dd88980d6cb9669
Angie Obando <angieobandoa@gmail.com>
2022-08-26 10:43:42 -0400

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+#funcion suma
+def sumar (num1,num2):
+    return num1+num2
+
+#funcion resta
+def restar (num1,num2):
+    return num1-num2
+
+#funcion multiplicar
+def multiplicar(num1,num2):
+    return num1*num2
+
+#funcion dividir
+def dividir (num1,num2):
+    return num1/num2
---
+++ funciones.py
@@ -0,0 +1,13 @@
+import calculadora as cal
+
+#sumar 
+print cal.sumar
+
+#restar
+print cal.restar
+
+#multiplicar
+print cal.multiplicar
+
+#dividir
+print cal.dividir
