ba27700eb7f60d9471356f1308c63e8a952c5ea4
Angie Obando <angieobandoa@gmail.com>
2022-08-26 10:56:34 -0400

    cosas

--- funciones.py
+++ funciones.py
@@ -6,8 +6,13 @@ print (cal.sumar(4,8))
 #restar
 print (cal.restar(8,3))
 
+#holaaaaaa
 #multiplicar
-print (cal.multiplicar(6,7))
+print (cal.multiplicar(6,7)) 
+jdsjdfosij
 
+
+#holaaaaa
 #dividir
 print (cal.dividir(2,5))
+poijfioejgsoki
