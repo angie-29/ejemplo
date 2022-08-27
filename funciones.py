f19eef1c8d1b2a26d9fcd30c61fa32fa090b326d
Angie Obando <angieobandoa@gmail.com>
2022-08-27 17:05:15 -0400

    final calculadora

--- funciones.py
+++ funciones.py
@@ -6,13 +6,8 @@ print (cal.sumar(4,8))
 #restar
 print (cal.restar(8,3))
 
-#holaaaaaa
 #multiplicar
 print (cal.multiplicar(6,7)) 
-jdsjdfosij
 
-
-#holaaaaa
 #dividir
 print (cal.dividir(2,5))
-poijfioejgsoki
